6c3f3e3b0c310f8e8a9a91f0bd1aaac7538ca1e5
kadircan65 <kadircan65@gmail.com>
2026-02-26 16:32:13 +0300

    fix missing modules

---
+++ db.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL missing");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
